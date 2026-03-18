f8b7259880970afceb0296e720e4209d5d5cee36
Jonathan Taylor <79172404+CherryTreeGee811@users.noreply.github.com>
2026-03-18 15:59:02 -0400

    Rename variable _PROJECT_ROOT to _THIS_DIR

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ import yaml
 # ---------------------------------------------------------------------------
 # Paths
 # ---------------------------------------------------------------------------
-_PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+_THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+_PROJECT_ROOT = os.path.dirname(_THIS_DIR)
 if _PROJECT_ROOT not in sys.path:
     sys.path.insert(0, _PROJECT_ROOT)
 
